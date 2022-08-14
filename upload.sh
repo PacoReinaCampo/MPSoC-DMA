eb753cdf3b2662ba898f180090337a2d0ba12a07
PacoReinaCampo <frareicam@gmail.com>
2022-08-14 19:49:03 +0200

    Working in Peripheral-DMA 🔑

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 git config --global user.email "frareicam@gmail.com"
 git config --global user.name "PacoReinaCampo"
 git add *
-git commit -m "Working in Peripheral-DMA"
+git commit -m "Working in Peripheral-DMA 🔑"
 git push origin master
