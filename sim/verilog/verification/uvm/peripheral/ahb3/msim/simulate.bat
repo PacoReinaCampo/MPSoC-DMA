5c5c0d2e0f213ca4c938364ce82526c0a8799ef7
PacoReinaCampo <frareicam@gmail.com>
2023-03-29 19:24:50 +0200

    Working in Peripheral-DMA 🔑

--- sim/verilog/verification/uvm/peripheral/ahb3/msim/simulate.bat
+++ sim/verilog/verification/uvm/peripheral/ahb3/msim/simulate.bat
@@ -47,5 +47,5 @@ call ../../../../../../../settings64_msim.bat
 
 vlib work
 vlog -sv -stats=none -f system.vc
-vsim -c -do run.do work.testbench
+vsim -c -do run.do work.peripheral_uvm_testbench
 pause
